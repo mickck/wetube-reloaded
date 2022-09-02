45adf66a820b033176bee047d361fba40a29959c
Mickck <mikemwkim@gmail.com>
2022-09-02 23:39:56 +1000

    divided controllers and routers, using :id url

---
+++ README.md
@@ -0,0 +1,16 @@
+# Wetube Reloaded
+
+/ -> HOME
+/join -> Join
+/login -> Login
+/search -> Search
+
+/users/:id -> See User
+/users/logout -> Log out
+/users/edite -> Edit My Profile
+/users/delete -> Delete My Profile
+
+/videos/:id -> Watch Video
+/videos/:id/edit -> Edit Video
+/videos/:id/delete -> Delete Video
+/videos/upload -> Upload Video
--- src/server.js
+++ src/server.js
@@ -1,20 +1,19 @@
 import express from 'express';
 // const express = require('express');
 import morgan from 'morgan';
+import globalRouter from './routers/globalRouter';
+import videoRouter from './routers/videoRouter';
+import userRouter from './routers/userRouter';
+
 const PORT = 4000;
 
 const app = express();
 const logger = morgan('dev');
-
-const home = (req, res) => {
-  return res.send('hello');
-};
-const login = (req, res) => {
-  return res.send('login');
-};
 app.use(logger);
-app.get('/', home);
-app.get('/login', login);
+
+app.use('/', globalRouter);
+app.use('/videos', videoRouter);
+app.use('/users', userRouter);
 
 const handleListening = () => console.log(`Server lstening on port http://localhost:${PORT} 🚀`);
 
